04461f498d049ec31b725558790a9f5111c1d3d3
Joshua Kenta Remulta <jkhirako@gmail.com>
2025-12-26 14:43:14 +0800

    removed inline style

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,36 +1,13 @@
+import './Notification.css'
 
 const Notification = ({message, type}) => {
-  const base = {
-    backgroundColor: 'lightgrey',
-    fontSize: '20px',
-    borderStyle: 'solid',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '10px'
-  }
-
-  const errorStyle = {
-    color: 'red'
-  }
-
-  const successStyle = {
-    color: 'green'
-  }
-
-  const styles = {
-    ...base,
-    ...(type =='error' ? errorStyle: successStyle)
-  }
-
-  if (message == null) {
-    return null
-  }
+  if (!message) return null;
 
   return (
-    <div style={styles}>
+    <div className={`notification ${type === 'error' ? 'error' : 'success'}`}>
       {message}
     </div>
-  )
+  );
 }
 
-export default Notification
+export default Notification;

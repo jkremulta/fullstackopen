82acc17f6358842dc2c9004bbe624fb8dd0fd44b
Joshua Kenta Remulta <jkhirako@gmail.com>
2026-01-06 23:23:38 +0800

    removed unused library

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Persons from './components/Persons'

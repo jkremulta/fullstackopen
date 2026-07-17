95db82bed17c42ee82605791187786d6d56d4ff4
Joshua Kenta Remulta <jkhirako@gmail.com>
2026-07-17 20:44:17 +0800

    indentation

--- part7/bloglist/client/src/components/Notification.jsx
+++ part7/bloglist/client/src/components/Notification.jsx
@@ -1,14 +1,14 @@
-import { Alert } from "@mui/material";
+import { Alert } from '@mui/material'
 const Notification = ({ message, type }) => {
   if (message === null) {
-    return null;
+    return null
   }
 
   return (
     <Alert style={{ marginTop: 10, marginBottom: 10 }} severity={type}>
       {message}
     </Alert>
-  );
-};
+  )
+}
 
-export default Notification;
+export default Notification

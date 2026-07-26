d5075bf74a13352986b77a9d36e033d61cda0c78
Joshua Kenta Remulta <jkhirako@gmail.com>
2026-07-26 15:21:05 +0800

    chapter 3

--- part8/fs-graphql/library-frontend/src/components/Authors.jsx
+++ part8/fs-graphql/library-frontend/src/components/Authors.jsx
@@ -1,8 +1,30 @@
+import { useMutation, useQuery } from "@apollo/client/react";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+import { useState } from "react";
+
 const Authors = (props) => {
+  const { loading, data } = useQuery(ALL_AUTHORS);
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    editAuthor({ variables: { name, setBornTo: Number(born) } });
+    setName("");
+    setBorn("");
+  };
 
   return (
     <div>
@@ -14,7 +36,7 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {data.allAuthors.map((a) => (
             <tr key={a.id}>
               <td>{a.name}</td>
               <td>{a.born}</td>
@@ -23,8 +45,33 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
-    </div>
-  )
-}
 
-export default Authors
+      <h2>Set birthyear</h2>
+      <div>
+        <form onSubmit={submit}>
+          <select
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          >
+            <option value="">--Please choose an option--</option>
+            {data.allAuthors.map((a) => (
+              <option key={a.id} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+          <div>
+            born
+            <input
+              type="number"
+              onChange={(event) => setBorn(event.target.value)}
+            />
+          </div>
+          <button type="submit">update author</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Authors;
--- part8/fs-graphql/library-frontend/src/components/Books.jsx
+++ part8/fs-graphql/library-frontend/src/components/Books.jsx
@@ -1,9 +1,16 @@
+import { useQuery } from "@apollo/client/react";
+import { ALL_BOOKS } from "../queries";
+
 const Books = (props) => {
+  const { loading, data } = useQuery(ALL_BOOKS);
+
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  if (loading) {
+    return <div>loading....</div>;
+  }
 
   return (
     <div>
@@ -16,7 +23,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {data.allBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author}</td>
@@ -26,7 +33,7 @@ const Books = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
